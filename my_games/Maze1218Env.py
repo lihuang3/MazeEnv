e12dbf920ba94b1a9331d09bce180afa39244ece
lihuang3 <lihuang.mech@gmail.com>
2019-01-09 20:25:43 -0600

    update

--- my_games/Maze1218Env.py
+++ my_games/Maze1218Env.py
@@ -121,12 +121,12 @@ class Maze1218Env(core.Env):
             reward += 16
         elif max_cost_agent <= 12 * self.goal_range and not self.reward_grad[3]:
             self.reward_grad[3] = 1
+            done = True
+            return done, reward 
             reward += 16
         elif max_cost_agent <= 16 * self.goal_range and not self.reward_grad[4]:
             self.reward_grad[4] = 1
             reward += 8        
-            done = True
-            return done, reward 
         elif max_cost_agent <= 20 * self.goal_range and not self.reward_grad[10]:
             self.reward_grad[10] = 1
             reward += 8
