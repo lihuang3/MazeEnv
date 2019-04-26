3d6a4129efeca47e174cf71bfef82391c579898b
lihuang3 <lihuang.mech@gmail.com>
2019-04-26 15:28:02 -0500

    Update MazeEnv.py

--- my_games/MazeEnv.py
+++ my_games/MazeEnv.py
@@ -16,29 +16,26 @@ from time import sleep
 plt.ion()
 
 
-
 class MazeEnv(core.Env):
     def __init__(self):
         global mazeData, costData, centerline, freespace, mazeHeight, mazeWidth, robot_marker
         dir_path = os.path.dirname(os.path.realpath(__file__))
 
         self.map_data_dir = dir_path+'/MapData'
-
         robot_marker = 150
-        self.goal_range = 20
+        self.goal_range = 10
         self.actions = [1, 2, 3, 4] # {up, down, left ,right}
-        self.action_dict = {0: (-1, 0), 1: (1, 0), 2: (-1, 1), 3: (1, 1)}  # {up, down, left ,right}
+        self.action_map = {0: (-1, 0), 1: (1, 0), 2: (-1, 1), 3: (1, 1)}  # {up, down, left ,right}
         self.n_actions = len(self.actions)
         self.action_space = spaces.Discrete(self.n_actions)
-        mazeData, costData, centerline, freespace = self._load_data(self.map_data_dir)
+        mazeData, costData, freespace = self._load_data(self.map_data_dir)
         mazeHeight, mazeWidth = mazeData.shape
         self.observation_space = spaces.Box(low=0, high=255, shape=(mazeHeight, mazeWidth, 1), dtype=np.uint8)
         self.seed()
         self.maze = np.ones((mazeHeight, mazeWidth))-mazeData
-        self.centerline = np.ones((mazeHeight, mazeWidth))-centerline
         self.freespace = np.ones((mazeHeight, mazeWidth))-freespace
-        # self.goal = np.array([73, 10])
-        self.goal = np.array([31, 52])
+        self.goal = np.array([73, 10])
+        # self.goal = np.array([31, 52])
         self.init_state = []
         self.reset()
 
@@ -48,90 +45,100 @@ class MazeEnv(core.Env):
 
 
     def _load_data(self, data_directory):
-        mazeData = np.loadtxt(data_directory + '/scaled_maze7.csv').astype(int)
-        freespace = np.loadtxt(data_directory + '/scaled_maze7_freespace.csv').astype(int)
-        costData = np.loadtxt(data_directory + '/scaled_maze7_costmap1.csv').astype(int)
-        centerline = np.loadtxt(data_directory + '/scaled_maze7_centerline.csv').astype(int)
-        return mazeData, costData, centerline, freespace
+        filename = 'scaled_maze7'
+
+        mazeData = np.loadtxt(data_directory + '/'+filename+'.csv').astype(int)
+        freespace = np.loadtxt(data_directory + '/'+filename+'_freespace.csv').astype(int)
+        costData = np.loadtxt(data_directory + '/' +filename+ '_costmap.csv').astype(int)
+        return mazeData, costData, freespace
 
     def _build_robot(self):
+
         row, col = np.nonzero(freespace)
-        self.reward_grad = np.zeros(7).astype(np.uint8)
-        # try:
-        #     self.init_state
-        #     self.init_state_img
-        #     self.state = self.init_state
-        #     self.state_img = self.init_state_img
-        # except AttributeError:
+        self.reward_grad = np.zeros(40).astype(np.uint8)
         self.robot_num = 64 #len(row)
         self.robot = random.sample(range(row.shape[0]), self.robot_num)
         self.state = np.zeros(np.shape(mazeData)).astype(int)
         self.state_img = np.copy(self.state)
+        self.loc = np.zeros([self.robot_num,2]).astype(np.int32)
         for i in range(self.robot_num):
+            self.loc[i, :] = row[self.robot[i]], col[self.robot[i]]
             self.state[row[self.robot[i]], col[self.robot[i]]] += robot_marker
             self.state_img[row[self.robot[i]]-1:row[self.robot[i]]+2,
-                col[self.robot[i]]-1:col[self.robot[i]]+2] = robot_marker*np.ones([3,3])
+                col[self.robot[i]]-1:col[self.robot[i]]+2] = robot_marker
 
         self.init_state = self.state
         self.init_state_img = self.state_img
-
-
-
         self.output_img = self.state_img + self.maze * 255
-
-        # return self.output_img
 
         return (np.expand_dims(self.output_img, axis=2))
 
     def step(self,action):
+        info = {}
 
-        next_direction, next_axis = self.action_dict[action]
-
-        next_state = np.roll(self.state, next_direction, axis=next_axis)
-
-        # Collision check
-        collision = np.logical_and(next_state, self.freespace)*next_state
-
-        next_state *= np.logical_xor(next_state, self.freespace)
-
-        # Move robots in the obstacle area back to previous grids and obtain the next state
-        ## Case 1: overlapping with population index
-        next_state += np.roll(collision, -next_direction, axis=next_axis)
-        ## Case 2: overlapping w/o population index (0: no robot; 1: robot(s) exits)
-        # next_state = np.logical_or(np.roll(collision, -next_direction, axis=next_axis), next_state).astype(int)
-
-        # next_state *= robot_marker   # Mark robot with intensity 150
-
-        row, col = np.nonzero(next_state)
-
+        dy, dx = self.action_map[action]
+        prev_loc = np.copy(self.loc)
+        self.loc = np.add(self.loc, np.array([dy, dx]))
+        collision = np.where(self.freespace[self.loc[:, 0], self.loc[:, 1]] == 1.0)
+        self.loc[collision, :] = prev_loc[collision, :]
         self.state_img  *= 0 # np.zeros([mazeHeight,mazeWidth])
-
-        for i in range(row.shape[0]):
-            self.state_img[row[i]-1:row[i]+2, col[i]-1:col[i]+2] = robot_marker * np.ones([3, 3])
-
-        self.state = next_state
-
+        for i in range(self.robot_num):
+            self.state_img[self.loc[i,0]-1:self.loc[i,0]+1, self.loc[i,1]-1:self.loc[i,1]+1] = robot_marker
         self.output_img = self.state_img + self.maze*255
+        done, reward = self.get_reward()
+        return(np.expand_dims(self.output_img,axis=2),reward,done,info)
 
-        state_cost_matrix = self.state * costData/ robot_marker
-        cost_to_go = np.sum(state_cost_matrix)
+    def get_reward(self):
+        cost_to_go = np.sum(costData[self.loc[:, 0], self.loc[:, 1]])
+        max_cost_agent = np.max(costData[self.loc[:, 0], self.loc[:, 1]])
 
         done = False
         reward = -.1
 
-        if cost_to_go <= self.goal_range * self.robot_num:
+        if max_cost_agent <= self.goal_range * self.robot_num:
             done = True
             reward = 100.0
-        elif cost_to_go <= 2*self.goal_range * self.robot_num and not self.reward_grad[0]:
+        elif max_cost_agent <= 2*self.goal_range * self.robot_num and not self.reward_grad[0]:
             self.reward_grad[0] = 1
-            reward = 20.0
-        elif cost_to_go <= 3*self.goal_range and not self.reward_grad[1]:
+            reward = 4
+        elif max_cost_agent <= 3*self.goal_range and not self.reward_grad[1]:
             self.reward_grad[1] = 1
-            reward = 5.0
+            reward = 4
+        elif max_cost_agent <= 4*self.goal_range and not self.reward_grad[2]:
+            self.reward_grad[2] = 1
+            reward = 4
+        elif max_cost_agent <= 6*self.goal_range and not self.reward_grad[3]:
+            self.reward_grad[3] = 1
+            reward = 2
+        elif max_cost_agent <= 8*self.goal_range and not self.reward_grad[4]:
+            self.reward_grad[4] = 1
+            reward = 2
+        elif max_cost_agent <= 10*self.goal_range and not self.reward_grad[5]:
+            self.reward_grad[5] = 1
+            reward = 2      
 
-        info = {}
+        if cost_to_go <= self.goal_range * self.robot_num:
+            reward = 8
+        elif cost_to_go <= 2*self.goal_range * self.robot_num and not self.reward_grad[20]:
+            self.reward_grad[20] = 1
+            reward = 4
+        elif cost_to_go <= 3*self.goal_range and not self.reward_grad[21]:
+            self.reward_grad[21] = 1
+            reward = 4
+        elif cost_to_go <= 4*self.goal_range and not self.reward_grad[22]:
+            self.reward_grad[22] = 1
+            reward = 4
+        elif cost_to_go <= 6*self.goal_range and not self.reward_grad[23]:
+            self.reward_grad[23] = 1
+            reward = 2
+        elif cost_to_go <= 8*self.goal_range and not self.reward_grad[24]:
+            self.reward_grad[24] = 1
+            reward = 2
+        elif cost_to_go <= 10*self.goal_range and not self.reward_grad[25]:
+            self.reward_grad[25] = 1
+            reward = 2
 
-        return(np.expand_dims(self.output_img,axis=2),reward,done,info)
+        return done, reward
 
     def render(self, mode = 'human'):
         # plt.imshow(self.state_img + self.maze*255, vmin=0, vmax=255)
@@ -139,6 +146,7 @@ class MazeEnv(core.Env):
         plt.show(False)
         plt.pause(0.0001)
         plt.gcf().clear()
+
 
     def reset(self):
         return self._build_robot()
